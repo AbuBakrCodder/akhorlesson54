0d4742a7e0745398d0ae76cfa73e069eafd9eed5
Abu Bakr <abubakrprogrammer11@gmail.com>
2025-11-04 19:47:54 +0500

    footer links changed

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -12,11 +12,11 @@ function MainLayout() {
         <Outlet />
       </main>
       <footer>
-        <a href="https://www.youtube.com/@akhrorweb" target="_blank">
-          Youtube
+        <a href="https://t.me/ABUBAKR_WEB" target="_blank">
+          Telegram
         </a>{" "}
         |
-        <a href="https://www.github.com/akhrorsoliev" target="_blank">
+        <a href="https://github.com/AbuBakrCodder" target="_blank">
           Github
         </a>
       </footer>
